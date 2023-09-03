6c996c981fb045d8a273ab5ee8ed784c1f4f0292
DivyaRevindran <73018114+DivyaRevindran@users.noreply.github.com>
2023-09-03 14:14:41 +0530

    Delete com/faith directory

--- com/faith/propel/java/camp4/Animals.java
+++
@@ -1,6 +0,0 @@
-package com.faith.propel.java.camp4;
-//question 3
-
-public abstract class Animals {
-	public abstract void saySomething();
-}
--- com/faith/propel/java/camp4/Isecond.java
+++
@@ -1,7 +0,0 @@
-package com.faith.propel.java.camp4;
-//question 2
-public interface Isecond {
-
-	public void dsiplay();
-	public void demo();
-}
